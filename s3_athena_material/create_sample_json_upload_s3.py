4da76f0d4128bc09a786abd64cbcbe8dbe3d4bfd
Keisuke Minami <lastshogun13@gmail.com>
2022-06-13 18:31:32 +0700

    fix s3_athena_material/create_sample_json_upload_s3.py

--- s3_athena_material/create_sample_json_upload_s3.py
+++ s3_athena_material/create_sample_json_upload_s3.py
@@ -10,8 +10,9 @@ s3_bucket_name = "keisuke-test-20220607"
 s3_path = datetime.datetime.now().strftime('%Y%m%d')
 file_name = datetime.datetime.now().strftime('%Y%m%d_%H%M%S.json')
 
-a1 = math.sin(datetime.datetime.now().hour * math.pi * 2 / 2 / 240)
-dist = random.lognormvariate(a1, a1 / 2)
+hour = datetime.datetime.now().hour
+a1 = math.sin(hour * math.pi / 24) + 1
+dist = random.lognormvariate(a1, a1 / 3)
 if dist <= 0.1:
   dist = 0.1
 
